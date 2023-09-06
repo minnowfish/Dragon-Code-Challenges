6eb56c6be5d0b426ae706b76eb6b12ef5ab68787
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2023-09-06 15:23:39 +0800

    Create AdvanceTask3.py

---
+++ AdvanceTask3.py
@@ -0,0 +1,6 @@
+medalists = ['Carl Lewis', 'Michael Phelps', 'Nadia Comăneci', 'Simone Biles', 'Usain Bolt']
+
+for i in range(len(medalists)):
+    if medalists[i]=="Simone Biles":
+        print(i)
+        break
