9fa2095ef3a3dd557005680b3eae47fe273eaab0
Ashlyn <141232366+minnowfish@users.noreply.github.com>
2023-09-06 15:23:09 +0800

    Create AdvanceTask1.py

---
+++ AdvanceTask1.py
@@ -0,0 +1,7 @@
+characters = ["Harry Potter", "Hermione Granger", "Ron Weasley", "Draco Malfoy", "Ginny Weasley",]
+sortedList = []
+for i in characters:
+    sortedList.append(i)
+sortedList.sort()
+print(characters)
+print(sortedList)
